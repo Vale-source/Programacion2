81d2af4cc0796e52b6efc9be72ca5c2af44b3272
Valentin Curiel <valentincuriel03@gmail.com>
2024-05-12 13:40:09 -0300

    Entrega del codigo de los 6 videos de Spring

---
+++ Persona/src/main/java/com/example/persona/entities/Persona.java
@@ -0,0 +1,32 @@
+package com.example.persona.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.envers.Audited;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "persona")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Audited
+public class Persona implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre")
+    private String name;
+    @Column(name = "apellido")
+    private String surname;
+    @Column(name = "dni")
+    private int dni;
+
+}
